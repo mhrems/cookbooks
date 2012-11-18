475f8f41ce3c1ed7d41b6ca180aba3bf24e3e7d3
mhrems <mhr.bond@gmail.com>
2012-11-18 15:01:16 +0900

    add gmond plugin

---
+++ gp_vm_stats/CHANGELOG.md
@@ -0,0 +1,12 @@
+# CHANGELOG for gp_vm_stats
+
+This file is used to list changes made in each version of gp_vm_stats.
+
+## 0.1.0:
+
+* Initial release of gp_vm_stats
+
+- - - 
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ gp_vm_stats/recipes/default.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook Name:: gp_vm_stats
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+template "/usr/lib64/ganglia/python_modules/vm_stats.py" do
+	source "vm_stats.py.erb"
+end
+
+
+template "/etc/ganglia/conf.d/vm_stats.pyconf" do
+	source "vm_stats.pyconf.erb"
+end
+
+case node["platform"]
+when "ubuntu", "debian"
+	service "ganglia-monitor" do
+	    action :start
+	end
+	
+when "redhat", "centos", "fedora"
+	service "gmond" do
+		service_name "gmond"
+	    action :start
+	end
+end
+
+
